879dc002b15cffc96ac748ff97095bacaec50a53
Yu Jizhu <yjz19950602@qq.com>
2020-04-16 14:44:08 +0800

    add sin and cos operator for scalar

---
+++ src/Core/math/Trigonometric.hpp
@@ -0,0 +1,102 @@
+/* Copyright (c) 2020 Yu Jizhu. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#ifndef METAAUTODIFF_TRIGONOMETRIC_HPP
+#define METAAUTODIFF_TRIGONOMETRIC_HPP
+
+#include"TrigonometricTrait.hpp"
+#include"PartialDerivativeTrait.hpp"
+#include"../util/Meta.hpp"
+
+namespace MetaAD {
+
+namespace ad_math {
+    template<typename InputValueTypePara>
+    struct sin_policy;
+
+    template<>
+    struct sin_policy<double> {
+        using OutputValueType = typename sin_trait<double>::type;
+        using DerivativeValueType = typename ad_math::partial_derivative_trait<double, double>::type;
+        template<unsigned int variableIndex> 
+        static typename std::enable_if<internal::is_same_value<unsigned int, variableIndex, 0>::value,
+                                       DerivativeValueType>::type
+        derivative(double operand) {
+            return std::cos(operand);
+        }
+
+        static OutputValueType compute(double operand) {
+            return std::sin(operand);
+        }
+    };
+
+    template<typename InputValueTypePara>
+    struct cos_policy;
+
+    template<>
+    struct cos_policy<double> {
+        using OutputValueType = typename cos_trait<double>::type;
+        using DerivativeValueType = typename ad_math::partial_derivative_trait<double, double>::type;
+        template<unsigned int variableIndex> 
+        static typename std::enable_if<internal::is_same_value<unsigned int, variableIndex, 0>::value,
+                                       DerivativeValueType>::type
+        derivative(double operand) {
+            return std::sin(operand);
+        }
+        
+        static OutputValueType compute(double operand) {
+            return std::cos(operand);
+        }
+    };
+
+/* //对矩阵的运算之后再加
+    template<>
+    struct add_policy<ad_MatrixXd, ad_MatrixXd> {
+        using OutputValueType = typename add_trait<ad_MatrixXd, ad_MatrixXd>::type;
+        using DerivativeValueType = typename ad_math::partial_derivative_trait<ad_MatrixXd, ad_MatrixXd>::type;
+        template<unsigned int variableIndex> 
+        static typename std::enable_if<internal::is_same_value<unsigned int, variableIndex, 0>::value
+                                    || internal::is_same_value<unsigned int, variableIndex, 1>::value,  
+                                       DerivativeValueType>::type
+        derivative(const ad_MatrixXd& leftOperand, const ad_MatrixXd& rightOperand, const OutputValueType& output) {
+            // 矩阵加法必然满足输入矩阵，输出矩阵的维数（行、列）相同。
+            auto rowLength = output.rows();
+            auto colLength = output.cols();
+            assert((rowLength == leftOperand.rows() && rowLength == rightOperand.rows()
+                 && colLength == leftOperand.cols() && colLength == rightOperand.cols()));
+            return ad_MatrixXd::Identity(rowLength*colLength, rowLength*colLength);
+        }
+        
+        // 由于Matrix加法要求两个输入矩阵维数相同，且它对第一个输入和对第二个输入的偏导数相同，因此只实现一个
+        // 下面的偏导数分支也可以用if constexpr来实现
+//        template<unsigned int variableIndex>
+//        static typename std::enable_if<internal::is_same_value<unsigned int, variableIndex, 1>::value,
+//                                       OutputValueType>::type 
+//        derivative(const ad_MatrixXd& leftOperand, const ad_MatrixXd& rightOperand, const OutputValueType& output) {
+//        	auto rowLength = output.rows();
+//       	auto colLength = output.cols();
+//            return ad_MatrixXd::Identity(rowLength*colLength, rowLength*colLength); 
+//        };
+        
+        static OutputValueType compute(const ad_MatrixXd& leftOperand, const ad_MatrixXd& rightOperand) {
+            assert((leftOperand.rows() == rightOperand.rows() && leftOperand.cols() == rightOperand.cols()));
+            return leftOperand + rightOperand;
+        }
+    };
+    */
+}
+
+}
+
+#endif
---
+++ src/Operator/TrigonometricNode.hpp
@@ -0,0 +1,226 @@
+/* Copyright (c) 2020 Yu Jizhu. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#ifndef METAAUTODIFF_TRIGONOMETRICNODE_HPP
+#define METAAUTODIFF_TRIGONOMETRICNODE_HPP
+
+#include"../Core/math/Trigonometric.hpp"
+#include"../Core/GraphNodeBase.hpp"
+#include"../Core/NodeWrapper.hpp"
+#include<memory>
+#include<tuple>
+
+namespace MetaAD {
+
+template<typename InputNodeTypePara>
+struct SinNodeTrait {
+    using InputNodeValueType = typename InputNodeTypePara::ValueType;
+    using type = typename ad_math::sin_trait<InputNodeValueType>::type;
+};
+
+template<typename InputNodeTypePara>
+struct SinPolicyDefault {
+    using InputNodeValueType = typename InputNodeTypePara::ValueType;
+    using type = typename ad_math::sin_policy<InputNodeValueType>;
+};
+
+template<typename InputNodeTypePara, unsigned int indexPara,
+         typename ValueTypePara = typename SinNodeTrait<InputNodeTypePara>::type, 
+         typename PolicyPara = typename SinPolicyDefault<InputNodeTypePara>::type,
+         bool legalPara = isNodeType<InputNodeTypePara>::value>
+class SinNodeImp;
+
+template<typename InputNodeTypePara, unsigned int indexPara, typename ValueTypePara, typename PolicyPara>
+class SinNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara, true>
+  : public GraphNodeBase<SinNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara,true>> {
+  public:
+    using Base = GraphNodeBase<SinNodeImp>;
+    using InputNodeTypes = std::tuple<std::shared_ptr<InputNodeTypePara>>;
+    using ValueType = ValueTypePara;
+    using Policy = PolicyPara;
+    using ConcreteNodeType = SinNodeImp;
+    static constexpr unsigned int index() {
+        return indexPara;
+    }
+
+    SinNodeImp() = delete;
+
+    SinNodeImp(std::shared_ptr<InputNodeTypePara> inputNode)
+      : Base(), inputs(InputNodeTypes(inputNode)) {
+        output = Policy::compute(inputNode->getValue());
+    }
+
+    //MatTransposeNodeImp(const MatTransposeNodeImp& rValue) = default;
+    //MatTransposeNodeImp(const MatTransposeNodeImp&& lvalue) = default;    //不确定要不要加移动构造函数，这要求所有的成员都具有移动构造函数
+    
+    // 拷贝赋值运算符
+    // MatTransposeNodeImp& operator=(const MatTransposeNodeImp& rValue) = default;
+
+    ValueType getValue() { return output; }   
+    ValueType getValue() const { return output; }   
+
+    ValueType compute() const {
+        ValueType result = Policy::compute(std::get<0>(inputs)->compute());
+        return result;
+    }
+
+    template<unsigned int variableIndex>
+    auto derivative() const;
+
+
+    template<unsigned int n>
+    friend struct ChainRuleExpansion;
+
+    template<unsigned int n>
+    friend struct ChainRuleExpansion_temp;
+  private:
+    InputNodeTypes inputs;
+    ValueType output;
+};
+
+template<typename InputNodeTypePara, unsigned int indexPara,
+         typename ValueTypePara, typename PolicyPara, bool legalPara>
+struct traits<SinNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara, legalPara>> {
+    using InputNodeTypes = std::tuple<std::shared_ptr<InputNodeTypePara>>;
+    using ValueType = ValueTypePara;
+    using Policy = PolicyPara;
+    static constexpr unsigned int index = indexPara;
+};
+
+
+template<typename ConcreteInputNodeTypePara, unsigned int indexPara = ConcreteInputNodeTypePara::index() + 1>
+auto sinImp(const std::shared_ptr<ConcreteInputNodeTypePara>& inputNodePtr) {
+    using NodeType = SinNodeImp<ConcreteInputNodeTypePara, indexPara>;
+    return std::shared_ptr<NodeType>(new NodeType(inputNodePtr));
+}
+
+template<typename ConcreteInputNodeTypePara>
+auto sin(const NodeWrapper<ConcreteInputNodeTypePara>& inputNode) {
+    auto concreteResultNodePtr = sinImp(inputNode.pNode);
+    using ConcreteResultNodeType = typename decltype(concreteResultNodePtr)::element_type;
+    return NodeWrapper<ConcreteResultNodeType>(concreteResultNodePtr);
+}
+
+
+
+// CosNode
+template<typename InputNodeTypePara>
+struct CosNodeTrait {
+    using InputNodeValueType = typename InputNodeTypePara::ValueType;
+    using type = typename ad_math::cos_trait<InputNodeValueType>::type;
+};
+
+template<typename InputNodeTypePara>
+struct CosPolicyDefault {
+    using InputNodeValueType = typename InputNodeTypePara::ValueType;
+    using type = typename ad_math::cos_policy<InputNodeValueType>;
+};
+
+template<typename InputNodeTypePara, unsigned int indexPara,
+         typename ValueTypePara = typename CosNodeTrait<InputNodeTypePara>::type, 
+         typename PolicyPara = typename CosPolicyDefault<InputNodeTypePara>::type,
+         bool legalPara = isNodeType<InputNodeTypePara>::value>
+class CosNodeImp;
+
+template<typename InputNodeTypePara, unsigned int indexPara, typename ValueTypePara, typename PolicyPara>
+class CosNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara, true>
+  : public GraphNodeBase<CosNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara,true>> {
+  public:
+    using Base = GraphNodeBase<CosNodeImp>;
+    using InputNodeTypes = std::tuple<std::shared_ptr<InputNodeTypePara>>;
+    using ValueType = ValueTypePara;
+    using Policy = PolicyPara;
+    using ConcreteNodeType = CosNodeImp;
+    static constexpr unsigned int index() {
+        return indexPara;
+    }
+
+    CosNodeImp() = delete;
+
+    CosNodeImp(std::shared_ptr<InputNodeTypePara> inputNode)
+      : Base(), inputs(InputNodeTypes(inputNode)) {
+        output = Policy::compute(inputNode->getValue());
+    }
+
+    //MatTransposeNodeImp(const MatTransposeNodeImp& rValue) = default;
+    //MatTransposeNodeImp(const MatTransposeNodeImp&& lvalue) = default;    //不确定要不要加移动构造函数，这要求所有的成员都具有移动构造函数
+    
+    // 拷贝赋值运算符
+    // MatTransposeNodeImp& operator=(const MatTransposeNodeImp& rValue) = default;
+
+    ValueType getValue() { return output; }   
+    ValueType getValue() const { return output; }   
+
+    ValueType compute() const {
+        ValueType result = Policy::compute(std::get<0>(inputs)->compute());
+        return result;
+    }
+
+    template<unsigned int variableIndex>
+    auto derivative() const;
+
+
+    template<unsigned int n>
+    friend struct ChainRuleExpansion;
+
+    template<unsigned int n>
+    friend struct ChainRuleExpansion_temp;
+  private:
+    InputNodeTypes inputs;
+    ValueType output;
+};
+
+template<typename InputNodeTypePara, unsigned int indexPara,
+         typename ValueTypePara, typename PolicyPara, bool legalPara>
+struct traits<CosNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara, legalPara>> {
+    using InputNodeTypes = std::tuple<std::shared_ptr<InputNodeTypePara>>;
+    using ValueType = ValueTypePara;
+    using Policy = PolicyPara;
+    static constexpr unsigned int index = indexPara;
+};
+
+template<typename ConcreteInputNodeTypePara, unsigned int indexPara = ConcreteInputNodeTypePara::index() + 1>
+auto cosImp(const std::shared_ptr<ConcreteInputNodeTypePara>& inputNodePtr) {
+    using NodeType = CosNodeImp<ConcreteInputNodeTypePara, indexPara>;
+    return std::shared_ptr<NodeType>(new NodeType(inputNodePtr));
+}
+
+template<typename ConcreteInputNodeTypePara>
+auto cos(const NodeWrapper<ConcreteInputNodeTypePara>& inputNode) {
+    auto concreteResultNodePtr = cosImp(inputNode.pNode);
+    using ConcreteResultNodeType = typename decltype(concreteResultNodePtr)::element_type;
+    return NodeWrapper<ConcreteResultNodeType>(concreteResultNodePtr);
+}
+
+template<typename InputNodeTypePara, unsigned int indexPara, typename ValueTypePara, typename PolicyPara>
+template<unsigned int variableIndex>
+auto SinNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara, true>::derivative() const {
+    static_assert(variableIndex == 0, "The partial derivative variable index must be 0.");
+    using DerivativeNodeType = CosNodeImp<InputNodeTypePara, indexPara + 1>; // +1是为了不让求导节点与原结果节点的类型相同
+    auto derivativeNodePtr = std::shared_ptr<DerivativeNodeType>(new DerivativeNodeType(std::get<0>(inputs)));
+    return derivativeNodePtr;
+}
+
+template<typename InputNodeTypePara, unsigned int indexPara, typename ValueTypePara, typename PolicyPara>
+template<unsigned int variableIndex>
+auto CosNodeImp<InputNodeTypePara, indexPara, ValueTypePara, PolicyPara, true>::derivative() const {
+    static_assert(variableIndex == 0, "The partial derivative variable index must be 0.");
+    using DerivativeNodeType = SinNodeImp<InputNodeTypePara, indexPara + 1>; // +1是为了不让求导节点与原结果节点的类型相同
+    auto derivativeNodePtr = std::shared_ptr<DerivativeNodeType>(new DerivativeNodeType(std::get<0>(inputs)));
+    return derivativeNodePtr;
+}
+
+}
+
+#endif
